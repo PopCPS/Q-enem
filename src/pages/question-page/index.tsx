12ce9674ebb6ed3c1661b1ff61f83c39e045562a
Artur Coelho <arturcoelho.cps@gmail.com>
2024-10-29 14:36:25 -0300

    fix/requer auth para questões

--- src/pages/question-page/index.tsx
+++ src/pages/question-page/index.tsx
@@ -3,19 +3,43 @@ import { ProgressBar } from "./progress-bar"
 import { QuestionsNav } from "./questions-nav"
 import { Question } from "./question"
 import { useAppDispatch, useAppSelector } from "../../store/hooks"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { set_questionIndex } from "../../store/reducers/dataReducer"
 import { LeaveModal } from "./leave-modal"
+import { api } from "../../lib/axios"
+import { useNavigate } from "react-router-dom"
 
 export const Questions = () => {
 
   const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+
+  const [ isAuth, setIsAuth ] = useState<boolean | null>(null)
 
   const isLeaveModalOpen = useAppSelector(state => state.apiData.isLeaveModalOpen)
 
+  const pingAuth = async () => {
+    await api.post('/api/ping')
+    .then(response => {
+      if(response.status === 200) {
+        setIsAuth(true)
+      }
+    })
+    .catch(() => {
+      setIsAuth(false)
+    })
+  }
+
   useEffect(() => {
+    pingAuth()
     dispatch(set_questionIndex(1))
   }, [])
+
+  useEffect(() => {
+    if(isAuth === false) {
+      navigate('/')
+    }
+  }, [ isAuth ])
 
   return (
     <>
